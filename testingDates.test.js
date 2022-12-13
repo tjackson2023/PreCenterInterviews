92ce7fee0997ed6a8a2b0d9d68f78e15df7744be
samronning <55357002+samronning@users.noreply.github.com>
2022-12-13 14:32:56 -0500

    updates to make more clear

--- testingDates.test.js
+++ testingDates.test.js
@@ -1,24 +1,25 @@
+//assume dateEqual uses Date.prototype.getTime() to compare dates
 import { dateEqual } from './helpers';
 
-function setDateTomorrow(date) {
+function setDatePlus1(date) {
     date.setDate(date.getDate() + 1);
     return date;
 }
 
-function setDateYesterday(date) {
+function setDateMinus1(date) {
     date.setDate(date.getDate() - 1);
     return date;
 }
 
 describe('dateFunctionTests', () => {
     const testDateInput = new Date();
-    it('returns tomorrow', () => {
-        const tomorrow = setDateTomorrow(testDateInput);
+    it('setDatePlus1 returns the next date (tomorrow)', () => {
+        const tomorrow = setDatePlus1(testDateInput);
         const expected = new Date().setDate(new Date().getDate() + 1);
         assert(dateEqual(tomorrow, expected));
     })
-    it('returns yesterday', () => {
-        const yesterday = setDateYesterday(testDateInput);
+    it('setDateMinus1 returns the previous date (yesterday)', () => {
+        const yesterday = setDateMinus1(testDateInput);
         const expected = new Date().setDate(new Date().getDate() - 1);
         assert(dateEqual(yesterday, expected));
     })
